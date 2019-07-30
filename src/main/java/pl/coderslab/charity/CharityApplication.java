8daba91a804fde8d0b7efad2b14bd8ff77702680
Bartłomiej Pietrzyk <bapietrzyk@gmail.com>
2019-07-30 02:00:46 +0200

    Initial Commit

---
+++ src/main/java/pl/coderslab/charity/CharityApplication.java
@@ -0,0 +1,15 @@
+package pl.coderslab.charity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@EnableJpaRepositories
+@SpringBootApplication
+public class CharityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CharityApplication.class, args);
+    }
+
+}
