65c7fe3a5f0d7b3d09a6e81a43f0e8d111f64c65
Bartłomiej Pietrzyk <bapietrzyk@gmail.com>
2019-08-01 18:14:29 +0200

    user entity

---
+++ src/main/java/pl/coderslab/charity/entity/User.java
@@ -0,0 +1,25 @@
+package pl.coderslab.charity.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Email;
+
+@Getter
+@Setter
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    @Email
+    private String email;
+    private String password;
+    private String passwordConfirm;
+}
