f9cec511990d15112e26d835c4157679d25a4b98
Bartłomiej Pietrzyk <bapietrzyk@gmail.com>
2019-08-02 16:55:28 +0200

    Admin panel created

---
+++ src/main/java/pl/coderslab/charity/controller/AdminPanelController.java
@@ -0,0 +1,17 @@
+package pl.coderslab.charity.controller;
+
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Secured("ROLE_ADMIN")
+@RequestMapping("/admin")
+public class AdminPanelController {
+
+    @GetMapping
+    public String showAdminPanel() {
+        return "admin/index";
+    }
+}
