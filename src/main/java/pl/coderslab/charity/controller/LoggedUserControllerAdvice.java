a929f7c37fea9104dcbb1f7bdccf2c0429098586
Bartłomiej Pietrzyk <bapietrzyk@gmail.com>
2019-08-02 13:53:29 +0200

    Logged user controllerAdvice

---
+++ src/main/java/pl/coderslab/charity/controller/LoggedUserControllerAdvice.java
@@ -0,0 +1,26 @@
+package pl.coderslab.charity.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import pl.coderslab.charity.entity.User;
+import pl.coderslab.charity.repository.UserRepository;
+
+import java.security.Principal;
+
+@ControllerAdvice
+public class LoggedUserControllerAdvice {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @ModelAttribute("loggedUser")
+    public String currentUserName(Principal principal) {
+        if (principal == null) {
+            return "";
+        }
+        User loggedUser = userRepository.findByEmail(principal.getName());
+
+        return loggedUser.getFirstName() + " " + loggedUser.getLastName();
+    }
+}
