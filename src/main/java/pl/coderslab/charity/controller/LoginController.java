06643de6d73e2f99433f5e7d9f482e1306b17f63
Bartłomiej Pietrzyk <bapietrzyk@gmail.com>
2019-08-02 13:54:52 +0200

    register form updated: errrors and messages

--- src/main/java/pl/coderslab/charity/controller/LoginController.java
+++ src/main/java/pl/coderslab/charity/controller/LoginController.java
@@ -1,21 +1,13 @@
 package pl.coderslab.charity.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import pl.coderslab.charity.repository.UserRepository;
 
 @Controller
 @RequestMapping("/login")
 public class LoginController {
-    private UserRepository userRepository;
-
-    @Autowired
-    public LoginController(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
 
     @GetMapping("/login")
     public String showLoginForm() {
