a0f17933d601c83296b2ffd03385d1314a542325
Bartłomiej Pietrzyk <bapietrzyk@gmail.com>
2019-08-03 19:29:44 +0200

    little fixes

--- src/main/java/pl/coderslab/charity/repository/UserRepository.java
+++ src/main/java/pl/coderslab/charity/repository/UserRepository.java
@@ -4,7 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pl.coderslab.charity.entity.User;
 
+import java.util.List;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByEmail(String email);
+
+    List<User> findAll();
 }
