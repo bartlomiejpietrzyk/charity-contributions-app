0f8c80834d2027b7dd29e2a38779d522ff64f854
Bartłomiej Pietrzyk <bapietrzyk@gmail.com>
2019-08-03 02:49:26 +0200

    spring security updated

--- src/main/java/pl/coderslab/charity/security/SecurityConfig.java
+++ src/main/java/pl/coderslab/charity/security/SecurityConfig.java
@@ -28,7 +28,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .authorizeRequests()
                 .antMatchers("/",
-                        "/form").permitAll()
+                        "/form**",
+                        "/registration**").permitAll()
                 .antMatchers("/user**").hasAnyRole("USER", "ADMIN")
                 .antMatchers("/admin**").access("hasRole('ADMIN')")
                 .anyRequest().authenticated()
