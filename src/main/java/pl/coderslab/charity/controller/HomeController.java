962ffa971f1941a8d6b7d45504a5563d3ffb273f
Bartłomiej Pietrzyk <bapietrzyk@gmail.com>
2019-08-05 01:56:38 +0200

    up

--- src/main/java/pl/coderslab/charity/controller/HomeController.java
+++ src/main/java/pl/coderslab/charity/controller/HomeController.java
@@ -2,14 +2,9 @@ package pl.coderslab.charity.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import pl.coderslab.charity.entity.Donation;
 import pl.coderslab.charity.repository.DonationRepository;
 import pl.coderslab.charity.repository.InstitutionRepository;
-
-import java.util.List;
 
 @Controller
 public class HomeController {
@@ -23,8 +18,7 @@ public class HomeController {
     }
 
     @GetMapping("/")
-    public String homeAction(Model model) {
-        model.addAttribute("institutions", institutionRepository.findAll());
+    public String homeAction() {
         return "user/index";
     }
 
@@ -33,20 +27,5 @@ public class HomeController {
         return "user/403";
     }
 
-    @ModelAttribute(name = "bags")
-    public Long showQuantityOfGivenBags() {
-        List<Donation> collect = donationRepository.findAll();
-        Long sum = 0L;
-        for (Donation donation : collect) {
-            sum += donation.getQuantity();
-        }
-        return sum;
-    }
-
-    @ModelAttribute(name = "charityOrg")
-    public Long charityOrganisationsQuantity() {
-        return institutionRepository
-                .count();
-    }
 
 }
