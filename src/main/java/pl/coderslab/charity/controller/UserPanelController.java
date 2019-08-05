8210a3ba17b9bac12b1affda73b02581f8876251
Bartłomiej Pietrzyk <bapietrzyk@gmail.com>
2019-08-05 04:36:33 +0200

    updated edit user

--- src/main/java/pl/coderslab/charity/controller/UserPanelController.java
+++ src/main/java/pl/coderslab/charity/controller/UserPanelController.java
@@ -1,19 +1,54 @@
 package pl.coderslab.charity.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.coderslab.charity.entity.User;
+import pl.coderslab.charity.repository.UserRepository;
+
+import javax.validation.Valid;
 
 @Controller
 @Secured("ROLE_USER")
 @RequestMapping("/user")
 public class UserPanelController {
+    private UserRepository userRepository;
+
+    @Autowired
+    public UserPanelController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @GetMapping
     public String showLoggedUserPage() {
         return "user/userPage";
     }
 
+    @GetMapping("/profile")
+    public String showUserProfileSite() {
 
+        return "user/userProfile";
+    }
+
+    @GetMapping("/edit")
+    public String showUserEditForm(@ModelAttribute("currentUser") User user, Model model) {
+        model.addAttribute("user", userRepository.getOne(user.getId()));
+        return "user/userEdit";
+    }
+
+    @PostMapping("/edit")
+    public String proceedUserEditForm(@ModelAttribute("user") @Valid User user,
+                                      BindingResult result) {
+        if (result.hasErrors()) {
+            return "redirect:/user/edit?id=" + user.getId() + "&failed";
+        }
+        userRepository.save(user);
+        return "redirect:/user/edit?success";
+    }
 }
