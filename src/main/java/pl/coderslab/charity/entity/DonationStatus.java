a0b48f06f17a8c80bd788295c06fcba89b908807
Bartłomiej Pietrzyk <bapietrzyk@gmail.com>
2019-08-06 16:10:04 +0200

    Donation Status

--- src/main/java/pl/coderslab/charity/entity/DonationStatus.java
+++ src/main/java/pl/coderslab/charity/entity/DonationStatus.java
@@ -2,14 +2,12 @@ package pl.coderslab.charity.entity;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
 
 @Data
 @Entity
+@Table
 public class DonationStatus {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
