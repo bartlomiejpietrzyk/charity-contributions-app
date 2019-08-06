0f7dac2ab6460abdffd5dc0f7d0ba569460fe597
Bartłomiej Pietrzyk <bapietrzyk@gmail.com>
2019-08-06 17:11:42 +0200

    user panel controller updated methods

--- src/main/java/pl/coderslab/charity/controller/UserPanelController.java
+++ src/main/java/pl/coderslab/charity/controller/UserPanelController.java
@@ -6,11 +6,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import pl.coderslab.charity.entity.Donation;
 import pl.coderslab.charity.entity.User;
+import pl.coderslab.charity.repository.DonationRepository;
 import pl.coderslab.charity.repository.UserRepository;
 import pl.coderslab.charity.service.UserService;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 @Secured("ROLE_USER")
@@ -18,11 +21,13 @@ import javax.validation.Valid;
 public class UserPanelController {
     private UserRepository userRepository;
     private UserService userService;
+    private DonationRepository donationRepository;
 
     @Autowired
-    public UserPanelController(UserRepository userRepository, UserService userService) {
+    public UserPanelController(UserRepository userRepository, UserService userService, DonationRepository donationRepository) {
         this.userRepository = userRepository;
         this.userService = userService;
+        this.donationRepository = donationRepository;
     }
 
     @GetMapping
@@ -49,5 +54,20 @@ public class UserPanelController {
         }
         userService.updateUser(user);
         return "redirect:/user/edit?id=" + user.getId() + "&success";
+    }
+
+    @GetMapping("/donations")
+    public String showDonationsList(Model model) {
+        return "user/userDonations";
+    }
+
+    @PostMapping("/donations")
+    public String proceedDonationList() {
+        return "redirect:/user/donations";
+    }
+
+    @ModelAttribute("donationsList")
+    public List<Donation> getDonationsList() {
+        return donationRepository.findAll();
     }
 }
