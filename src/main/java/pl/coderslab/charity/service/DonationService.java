e349f2c4aa9feb52106728cb7fca321941a7a5ff
Bartłomiej Pietrzyk <bapietrzyk@gmail.com>
2019-08-07 21:40:26 +0200

    DonationService updated method created

--- src/main/java/pl/coderslab/charity/service/DonationService.java
+++ src/main/java/pl/coderslab/charity/service/DonationService.java
@@ -5,20 +5,25 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import pl.coderslab.charity.entity.Donation;
 import pl.coderslab.charity.entity.User;
+import pl.coderslab.charity.repository.DonationRepository;
 import pl.coderslab.charity.repository.DonationStatusRepository;
 
 @Service
 @Transactional
 public class DonationService {
     private DonationStatusRepository donationStatusRepository;
+    private DonationRepository donationRepository;
 
     @Autowired
-    public DonationService(DonationStatusRepository donationStatusRepository) {
+    public DonationService(DonationStatusRepository donationStatusRepository, DonationRepository donationRepository) {
         this.donationStatusRepository = donationStatusRepository;
+        this.donationRepository = donationRepository;
     }
 
-    public void donationUpdate(Donation donation, User user) {
+    public void donationCreate(Donation donation, User user) {
         donation.setUser(user);
         donation.setStatus(donationStatusRepository.getOne(1L));
+        donationRepository.save(donation);
+
     }
 }
