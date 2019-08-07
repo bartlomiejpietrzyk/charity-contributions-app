8efa1002d16161107c1c3e89f98d868d9f082360
Bartłomiej Pietrzyk <bapietrzyk@gmail.com>
2019-08-07 14:35:15 +0200

    user donation pick up status date change when picked up

--- src/main/java/pl/coderslab/charity/service/UserService.java
+++ src/main/java/pl/coderslab/charity/service/UserService.java
@@ -4,19 +4,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import pl.coderslab.charity.entity.Donation;
 import pl.coderslab.charity.entity.User;
+import pl.coderslab.charity.repository.DonationRepository;
 import pl.coderslab.charity.repository.UserRepository;
+
+import java.time.LocalDate;
 
 @Service
 @Transactional
 public class UserService {
     private UserRepository userRepository;
     private PasswordEncoder passwordEncoder;
+    private DonationRepository donationRepository;
 
     @Autowired
-    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+    public UserService(UserRepository userRepository, DonationRepository donationRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
+        this.donationRepository = donationRepository;
     }
 
     public void updateUser(User user) {
@@ -24,5 +30,14 @@ public class UserService {
         user.setEnabled(userRepository.getOne(user.getId()).getEnabled());
         user.setRoles(user.getRoles());
         userRepository.save(user);
+    }
+
+    public void updateUserDonation(Donation donation) {
+        Donation existing = donationRepository.getOne(donation.getId());
+        if (donation.getStatus().getId() == 2) {
+            existing.setDate(LocalDate.now());
+        }
+        existing.setStatus(donation.getStatus());
+        donationRepository.save(existing);
     }
 }
