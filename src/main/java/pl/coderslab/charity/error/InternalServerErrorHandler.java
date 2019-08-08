e6d58ee358e13e04d13790e0007230d86a5434f4
Bartłomiej Pietrzyk <bapietrzyk@gmail.com>
2019-08-08 20:16:05 +0200

    error handlers

--- src/main/java/pl/coderslab/charity/error/InternalServerErrorHandler.java
+++ src/main/java/pl/coderslab/charity/error/InternalServerErrorHandler.java
@@ -14,20 +14,17 @@ import java.io.IOException;
 
 
 @ControllerAdvice
-public class ControllerExceptionHandler {
+public class InternalServerErrorHandler {
 
-    private static Logger log = LoggerFactory.getLogger(ControllerExceptionHandler.class);
+    private static Logger log = LoggerFactory.getLogger(InternalServerErrorHandler.class);
 
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler(Exception.class)
     public void notFoundHandler(HttpServletRequest httpServletRequest,
                                 HttpServletResponse httpServletResponse) throws IOException, ServletException {
-        {
-            log.debug("Item not found. HTTP 500 returned.");
+        log.debug("Item not found. HTTP 500 returned.");
 
-            httpServletResponse.sendRedirect(httpServletRequest.getContextPath() + "/500");
-
-        }
+        httpServletResponse.sendRedirect(httpServletRequest.getContextPath() + "/500");
 
     }
 }
--- src/main/java/pl/coderslab/charity/error/MyAccessDeniedHandler.java
+++ src/main/java/pl/coderslab/charity/error/MyAccessDeniedHandler.java
@@ -6,14 +6,14 @@ import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.access.AccessDeniedHandler;
-import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@Component
+@ControllerAdvice
 public class MyAccessDeniedHandler implements AccessDeniedHandler {
 
     private static Logger logger = LoggerFactory.getLogger(MyAccessDeniedHandler.class);
