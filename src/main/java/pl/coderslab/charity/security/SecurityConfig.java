8641b3f4f9d68812bc8876633fec53debf0eecb4
Bartłomiej Pietrzyk <bapietrzyk@gmail.com>
2019-08-09 14:00:55 +0200

    endpoint /lostPassword added

--- src/main/java/pl/coderslab/charity/security/SecurityConfig.java
+++ src/main/java/pl/coderslab/charity/security/SecurityConfig.java
@@ -31,7 +31,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/registration**", "/user/*/enable", "/403", "/404", "/500").permitAll()
+                .antMatchers("/", "/registration**", "/lostPassword", "/user/*/enable", "/403", "/404", "/500").permitAll()
                 .antMatchers("/user**").hasAnyRole("USER", "ADMIN")
                 .antMatchers("/admin**").access("hasRole('ADMIN')")
                 .anyRequest().authenticated()
