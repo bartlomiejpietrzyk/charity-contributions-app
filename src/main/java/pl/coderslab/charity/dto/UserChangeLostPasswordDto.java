67c227ee163487d8a64abd5a489dcda2e0b8087c
Bartłomiej Pietrzyk <bapietrzyk@gmail.com>
2019-08-11 13:51:07 +0200

    user change profile password dto created

---
+++ src/main/java/pl/coderslab/charity/dto/UserChangeLostPasswordDto.java
@@ -0,0 +1,55 @@
+package pl.coderslab.charity.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.validator.constraints.Length;
+import org.springframework.beans.factory.annotation.Autowired;
+import pl.coderslab.charity.entity.User;
+import pl.coderslab.charity.validator.FieldMatch;
+import pl.coderslab.charity.validator.ValidPassword;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+@ToString
+@Setter
+@Getter
+@FieldMatch.List({
+        @FieldMatch(first = "password", second = "passwordConfirm", message = "The password fields must match"),
+})
+public class UserChangeLostPasswordDto {
+    @NotEmpty
+    private String id;
+    @NotEmpty
+    @Length(min = 1)
+    private String firstName;
+    @NotEmpty
+    @Length(min = 1)
+    public String lastName;
+    @Email
+    @NotEmpty
+    public String email;
+    @Length(min = 8, max = 32)
+    @NotEmpty
+    @ValidPassword
+    public String password;
+    @Length(min = 8, max = 32)
+    @NotEmpty
+    public String passwordConfirm;
+    @NotEmpty
+    public String token;
+
+    @Autowired
+    public UserChangeLostPasswordDto() {
+    }
+
+    public UserChangeLostPasswordDto(User that) {
+        this.id = String.valueOf(that.getId());
+        this.firstName = that.getFirstName();
+        this.lastName = that.getLastName();
+        this.email = that.getEmail();
+        this.password = that.getPassword();
+        this.token = getToken();
+    }
+}
--- src/main/java/pl/coderslab/charity/dto/UserChangePasswordDto.java
+++ src/main/java/pl/coderslab/charity/dto/UserChangePasswordDto.java
@@ -2,53 +2,20 @@ package pl.coderslab.charity.dto;
 
 import lombok.Getter;
 import lombok.Setter;
-import lombok.ToString;
-import org.hibernate.validator.constraints.Length;
 import org.springframework.beans.factory.annotation.Autowired;
-import pl.coderslab.charity.entity.User;
 import pl.coderslab.charity.validator.FieldMatch;
-import pl.coderslab.charity.validator.ValidPassword;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotEmpty;
-
-@ToString
 @Setter
 @Getter
 @FieldMatch.List({
         @FieldMatch(first = "password", second = "passwordConfirm", message = "The password fields must match"),
 })
 public class UserChangePasswordDto {
-    @NotEmpty
     private String id;
-    @NotEmpty
-    @Length(min = 1)
-    private String firstName;
-    @NotEmpty
-    @Length(min = 1)
-    public String lastName;
-    @Email
-    @NotEmpty
-    public String email;
-    @Length(min = 8, max = 32)
-    @NotEmpty
-    @ValidPassword
-    public String password;
-    @Length(min = 8, max = 32)
-    @NotEmpty
-    public String passwordConfirm;
-    @NotEmpty
-    public String token;
+    private String password;
+    private String passwordConfirm;
+
     @Autowired
     public UserChangePasswordDto() {
-    }
-
-    public UserChangePasswordDto(User that) {
-        this.id = String.valueOf(that.getId());
-        this.firstName = that.getFirstName();
-        this.lastName = that.getLastName();
-        this.email = that.getEmail();
-        this.password = that.getPassword();
-        this.token = getToken();
     }
 }
