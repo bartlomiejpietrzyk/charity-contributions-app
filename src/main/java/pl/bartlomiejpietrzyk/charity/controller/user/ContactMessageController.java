61d115c2c01b66da01f08405ae0c4e71c04c3db5
Bartłomiej Pietrzyk <bapietrzyk@gmail.com>
2019-09-02 00:27:07 +0200

    message controller up

--- src/main/java/pl/bartlomiejpietrzyk/charity/controller/user/ContactMessageController.java
+++ src/main/java/pl/bartlomiejpietrzyk/charity/controller/user/ContactMessageController.java
@@ -6,16 +6,20 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import pl.bartlomiejpietrzyk.charity.dto.UserContactDto;
+import pl.bartlomiejpietrzyk.charity.email.EmailServiceImpl;
+import pl.bartlomiejpietrzyk.charity.entity.Message;
 import pl.bartlomiejpietrzyk.charity.service.MessageService;
 
 @Controller
 public class ContactMessageController {
 
     private final MessageService messageService;
+    private final EmailServiceImpl emailService;
 
     @Autowired
-    public ContactMessageController(MessageService messageService) {
+    public ContactMessageController(MessageService messageService, EmailServiceImpl emailService) {
         this.messageService = messageService;
+        this.emailService = emailService;
     }
 
     @ModelAttribute("contactMessage")
@@ -28,7 +32,14 @@ public class ContactMessageController {
         if (result.hasErrors()) {
             return "redirect:/?messageNotSend";
         }
-        messageService.sendMessage(userContactDto);
+        Message message = messageService.sendMessage(userContactDto);
+        emailService.sendSimpleMessage(userContactDto.getContactEmail(), userContactDto.getContactTitle(),
+                "Charity Contributions" +
+                        "\nPotwierdzenie wysłania wiadomości." +
+                        "\nID: " + message.getId() +
+                        "\nTytuł: " + userContactDto.getContactTitle() +
+                        "\nWiadomość: " + userContactDto.getContactMessage() +
+                        "\nW przeciągu 48h odpowiemy na Twoje zgłoszenie!");
         return "redirect:/?messageSent";
 
     }
