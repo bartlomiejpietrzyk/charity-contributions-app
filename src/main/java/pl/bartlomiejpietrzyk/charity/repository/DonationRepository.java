0bc528d20fe70b8d4bbc372ea58acaa5c3cb6349
Bartłomiej Pietrzyk <bapietrzyk@gmail.com>
2019-09-03 00:27:52 +0200

    added query

--- src/main/java/pl/bartlomiejpietrzyk/charity/repository/DonationRepository.java
+++ src/main/java/pl/bartlomiejpietrzyk/charity/repository/DonationRepository.java
@@ -1,5 +1,7 @@
 package pl.bartlomiejpietrzyk.charity.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pl.bartlomiejpietrzyk.charity.entity.Donation;
@@ -11,9 +13,11 @@ import java.util.List;
 public interface DonationRepository extends JpaRepository<Donation, Long> {
     Long countAllByUser(User user);
 
-    List<Donation> findAllByUser(User user);
+    Page<Donation> findAllByUser(User user, Pageable pageable);
 
     Long countUserDonationsByStatusNameEqualsAndUserId(String status, Long id);
 
     Long countUserQuantityDonationsByStatusNameEqualsAndUserId(String status, Long id);
+
+    List<Donation> findAllByUser(User user);
 }
