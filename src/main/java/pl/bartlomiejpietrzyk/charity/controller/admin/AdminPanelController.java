754e0e56579cf2736bbdc98c986c8c1fd7b431ab
Bartłomiej Pietrzyk <bapietrzyk@gmail.com>
2019-09-05 18:21:59 +0200

    dashboard controller added features

--- src/main/java/pl/bartlomiejpietrzyk/charity/controller/admin/AdminPanelController.java
+++ src/main/java/pl/bartlomiejpietrzyk/charity/controller/admin/AdminPanelController.java
@@ -7,7 +7,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.bartlomiejpietrzyk.charity.entity.Donation;
+import pl.bartlomiejpietrzyk.charity.entity.User;
 import pl.bartlomiejpietrzyk.charity.repository.DonationRepository;
+import pl.bartlomiejpietrzyk.charity.repository.UserRepository;
 
 import java.util.Comparator;
 import java.util.stream.Collectors;
@@ -17,10 +19,12 @@ import java.util.stream.Collectors;
 @RequestMapping("/admin")
 public class AdminPanelController {
     private final DonationRepository donationRepository;
+    private final UserRepository userRepository;
 
     @Autowired
-    public AdminPanelController(DonationRepository donationRepository) {
+    public AdminPanelController(DonationRepository donationRepository, UserRepository userRepository) {
         this.donationRepository = donationRepository;
+        this.userRepository = userRepository;
     }
 
     @GetMapping
@@ -31,6 +35,13 @@ public class AdminPanelController {
                 .stream().sorted(Comparator.comparing(Donation::getId).reversed())
                 .limit(5)
                 .collect(Collectors.toList()));
+        model.addAttribute("lastUsers", userRepository.findAll()
+                .stream()
+                .sorted(Comparator.comparing(User::getId).reversed())
+                .limit(5)
+                .collect(Collectors.toList()));
+        model.addAttribute("givenDonations");
+        model.addAttribute("toGiveDonations");
 
         return "admin/index";
     }
